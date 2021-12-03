d05480d82ccf680609f68d4639f386b47daf1265
Anastasiia Kitlinska <anastasiia.kitlinska.ext@blablacar.com>
2021-12-03 19:11:53 +0200

    added dummy tests and allure report

---
+++ src/test/java/org/example/TestClass.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class TestClass {
+
+    @Test
+    public void testOne(){
+        assertThat(true).isTrue();
+    }
+
+    @Test
+    public void testTwo(){
+        assertThat(true).isFalse();
+    }
+}
